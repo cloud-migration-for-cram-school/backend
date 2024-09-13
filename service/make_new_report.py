b845f87f53e3bf2325a9d92d1a52930090f0a65f
YYYY <ysaq3424@gmail.com>
2024-09-13 21:15:13 +0900

    update : fileIDを動的に処理するように修正
    次の方針
    基準となる変数'position'から相対値をJsonファイルに記述して処理ができるように変更する

--- service/make_new_report.py
+++ service/make_new_report.py
@@ -11,12 +11,13 @@ newsheet_setting_path = os.getenv('NEWSHEET_SETTING')
 NUMBER_OF_SHEET = 10 # 新しく作るシートの枚数
 
 class MakeNewReport(SpreadsheetService):
-    def __init__(self, fileID=None, position=None):
+    def __init__(self, fileID=None, sheetID=None, position=None):
         super().__init__(fileID)
         self.fileID = fileID
         self.service = build('sheets', 'v4', credentials=self.credentials)
         self.newsheet_setting = load_json(newsheet_setting_path)
         self.position = position
+        self.sheetID = sheetID
     
     def copy_template(self):
         """
@@ -28,20 +29,28 @@ class MakeNewReport(SpreadsheetService):
         """
         Jsonファイルを読み込み、セル結合、セルの塗りつぶしを反映する
         """
-        pass
-    
-    def debug_test(self):
+        self.molding_json()  # JSONを整形
         # batchUpdateリクエストの送信
         self.service.spreadsheets().batchUpdate(
             spreadsheetId=self.fileID,
             body=self.newsheet_setting
         ).execute()
-        print("Success")
+    
+    def molding_json(self):
+        """
+        Jsonファイルを整形する関数
+        """
+        for request in self.newsheet_setting["requests"]:
+            if "range" in request.get("mergeCells", {}):
+                request["mergeCells"]["range"]["sheetId"] = self.sheetID
+            elif "range" in request.get("repeatCell", {}):
+                request["repeatCell"]["range"]["sheetId"] = self.sheetID
 
 if __name__ == '__main__':
     URL = '1NjVsRdbP1sQPgllo9wGYfpbmKjSWQ3us4_dMljvWN14'
-    test = MakeNewReport(fileID=URL)
-    test.debug_test()
+    test = MakeNewReport(fileID=URL, sheetID=1300349524)
+    test.apply_json_to_sheet()  # 整形後にAPIに送信
+
 
 """
 ガチの自分用のメモ
