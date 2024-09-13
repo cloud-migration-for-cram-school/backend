0df95680608db3be8e824eb950b57582551cab72
YYYY <ysaq3424@gmail.com>
2024-09-13 18:19:21 +0900

    update : セルの塗りつぶしの設定を追加
    その他メソッドを追加
    動作未確認

--- service/make_new_report.py
+++ service/make_new_report.py
@@ -6,6 +6,7 @@ from googleapiclient.discovery import build
 
 load_dotenv()
 TEMPLATE_ID = os.getenv('TEMPLATE_ID')
+NEWSHEET_SETTING_PATH = os.getenv('NEWSHEET_SETTING')
 
 NUMBER_OF_SHEET = 10 # 新しく作るシートの枚数
 
@@ -13,8 +14,20 @@ class MakeNewReport(SpreadsheetService):
     def __init__(self, fileID=None, position=None):
         super().__init__(fileID)
         self.service = build('sheets', 'v4', credentials=self.credentials)
+        self.newsheet_setting = load_json(NEWSHEET_SETTING_PATH)
         self.position = position
     
+    def copy_template(self):
+        """
+        テンプレートをコピーして貼り付ける処理をする
+        """
+        pass
+
+    def apply_json_to_sheet(self):
+        """
+        Jsonファイルを読み込み、セル結合、セルの塗りつぶしを反映する
+        """
+        pass
     
 
 """
